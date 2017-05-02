b5d972e0dbcd3a67f2eb3016e23b5df1f08aadd7
Cyb3rghost <cyb3rghostx@gmail.com>
2017-05-03 02:41:40 +0400

    formulaire connexion interface

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <!-- Custom CSS -->
     <style>
     body {
-        padding-top: 150px;
+        padding-top: 180px;
         background-color: #1BBC9B;
         /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
     }
@@ -38,29 +38,49 @@
     <div class="container">
 
         <div class="row">
-            <div class="col-md-2">
+            <div class="col-md-3">
 
-                <img src="images/mail_logo.png" class="img-responsive" alt="Responsive image">
+                <img src="images/mail_logo.png" class="img-responsive" alt="Responsive image"> <br>
+                <font color="white">Bienvenue sur <b>MeliMailo</b> <br>
+                <hr>
+                Votre première application mail décentralisé inter-connecté.</font>
 
             </div>
-            <div class="col-md-8">
+            <div class="col-md-9">
                 
 
                 <div class="well">
-
-
-
+                    <br>
+                    <br>
+                    <br>
+                    <form class="form-horizontal">
+                      <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Adresse : </label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Mot de passe : </label>
+                        <div class="col-sm-10">
+                          <input type="password" class="form-control" id="inputPassword3" placeholder="Mot de passe">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" class="btn btn-success btn-block btn-sm">Connexion</button>
+                          <a class="btn btn-success btn-sm btn-block" href="index.php?inscription" role="button">Inscription</a>
+                        </div>
+                      </div>
+                    </form>
+                    <br>
+                    <br>
 
                 </div>
 
 
 
-            </div>
-            <div class="col-md-2">
-
-
-
-            </div>
+            </div>  
         </div>
         <!-- /.row -->
 
