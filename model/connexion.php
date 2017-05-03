259ef4589d6c5d7902fa5d9a1204084c9de66b52
Cyb3rghost <cyb3rghostx@gmail.com>
2017-05-03 13:25:04 +0400

    amélioration interface apres connexion

--- model/connexion.php
+++ model/connexion.php
@@ -1,7 +1,7 @@
 <?php
     // Connexion à la base de données
     try {
-        $bdd = new PDO('mysql:host=localhost;dbname=', '', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+        //$bdd = new PDO('mysql:host=localhost;dbname=', '', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
     }
     catch(Exception $e){
         die('Erreur : '.$e->getMessage());
